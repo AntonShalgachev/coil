2f360809a847b43e7f8cc569e5c31f51a1825b72
Anton Shalgachev <anton.shalgachev@gmail.com>
2021-11-07 22:43:34 +0100

    shorten namespace declaration

--- coil/include/coil/detail/ArgumentTraits.h
+++ coil/include/coil/detail/ArgumentTraits.h
@@ -2,20 +2,17 @@
 #include "coil/Context.h"
 #include "coil/NamedArgs.h"
 
-namespace coil
+namespace coil::detail
 {
-    namespace detail
-    {
-        template<typename T>
-        struct IsExplicitTargetArgument : std::is_pointer<T> {};
+    template<typename T>
+    struct IsExplicitTargetArgument : std::is_pointer<T> {};
 
-        template<typename T>
-        struct IsContextArgument : std::is_same<T, Context&> {};
+    template<typename T>
+    struct IsContextArgument : std::is_same<T, Context&> {};
 
-        template<typename T>
-        struct IsNamedArgsArgument : std::is_same<T, NamedArgs> {};
+    template<typename T>
+    struct IsNamedArgsArgument : std::is_same<T, NamedArgs> {};
 
-        template<typename T>
-        struct IsUserArgument : std::negation<std::disjunction<IsExplicitTargetArgument<T>, IsContextArgument<T>, IsNamedArgsArgument<T>>> {};
-    }
+    template<typename T>
+    struct IsUserArgument : std::negation<std::disjunction<IsExplicitTargetArgument<T>, IsContextArgument<T>, IsNamedArgsArgument<T>>> {};
 }
