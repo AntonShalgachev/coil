059f478ea9ca5c2566efcb4004bc03cea7c60f3b
Anton Shalgachev <anton.shalgachev@gmail.com>
2021-12-09 23:09:02 +0100

    use simpler template magic for non-user arguments

--- coil/include/coil/detail/FuncTraitsEx.h
+++ coil/include/coil/detail/FuncTraitsEx.h
@@ -1,21 +1,55 @@
 #pragma once
-#include "coil/utils/TypeListTraits.h"
-#include "ArgumentTraits.h"
-#include "coil/utils/FilterTypes.h"
+#include "coil/Context.h"
+#include "coil/NamedArgs.h"
 
 namespace coil::detail
 {
+    template<typename Types, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl {};
+
+    template<typename T, typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<T*, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, true, T, HasContext, HasNamedArgs> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<Context, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, true, HasNamedArgs> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<Context&, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, true, HasNamedArgs> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<Context const&, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, true, HasNamedArgs> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<NamedArgs, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, HasContext, true> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<NamedArgs&, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, HasContext, true> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<NamedArgs const&, Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs> : public ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, HasContext, true> {};
+
+    template<typename... Args, bool HasTarget, typename ExplicitTargetT, bool HasContext, bool HasNamedArgs>
+    struct ArgumentTraitsImpl<utils::Types<Args...>, HasTarget, ExplicitTargetT, HasContext, HasNamedArgs>
+    {
+        using UserArgumentTypes = utils::Types<Args...>;
+        using ExplicitTargetType = ExplicitTargetT;
+        static bool constexpr hasTarget = HasTarget;
+        static bool constexpr hasContext = HasContext;
+        static bool constexpr hasNamedArgs = HasNamedArgs;
+    };
+
     template<typename Func>
     struct FuncTraitsEx : public utils::FuncTraits<Func>
     {
     private:
         using Base = utils::FuncTraits<Func>;
-        using ArgTypes = typename Base::ArgumentTypes;
+        using ArgumentTraits = ArgumentTraitsImpl<typename Base::ArgumentTypes, false, void, false, false>;
 
     public:
-        using UserArgumentTypes = utils::FilterTypesT<IsUserArgument, ArgTypes>;
-        using ExplicitTargetTraits = utils::TypeListTraits<IsExplicitTargetArgument, ArgTypes>;
-        using ContextTraits = utils::TypeListTraits<IsContextArgument, ArgTypes>;
-        using NamedArgsTraits = utils::TypeListTraits<IsNamedArgsArgument, ArgTypes>;
+        using UserArgumentTypes = typename ArgumentTraits::UserArgumentTypes;
+        using ExplicitTargetType = typename ArgumentTraits::ExplicitTargetType;
+        static bool constexpr hasTarget = ArgumentTraits::hasTarget;
+        static bool constexpr hasContext = ArgumentTraits::hasContext;
+        static bool constexpr hasNamedArgs = ArgumentTraits::hasNamedArgs;
     };
 }
