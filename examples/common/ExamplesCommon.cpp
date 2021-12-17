8c9c39fe5eb1ed7d9d2abe917359b03ff0b2cc8d
Anton Shalgachev <anton.shalgachev@gmail.com>
2021-12-17 23:31:14 +0100

    add colors to the examples

--- examples/common/ExamplesCommon.cpp
+++ examples/common/ExamplesCommon.cpp
@@ -2,21 +2,35 @@
 
 #include <iostream>
 #include "coil/Bindings.h"
+#include "termcolor.hpp"
+
+namespace
+{
+    auto headerColor = &termcolor::cyan<char>;
+    auto errorColor = &termcolor::red<char>;
+    auto commandColor = &termcolor::bright_yellow<char>;
+    auto returnColor = &termcolor::green<char>;
+}
 
 void common::printSectionHeader(std::string_view header)
 {
+    std::cout << headerColor;
+    std::cout << std::string(header.length(), '=') << std::endl;
     std::cout << header << std::endl;
     std::cout << std::string(header.length(), '=') << std::endl;
+    std::cout << termcolor::reset;
 }
 
 void common::executeCommand(coil::Bindings& bindings, std::string_view command)
 {
-    std::cout << "Executing [" << command << "]" << std::endl;
+    std::cout << "Executing [" << commandColor << command << termcolor::reset << "]" << std::endl;
     auto result = bindings.execute(command);
 
     for (const auto& error : result.errors)
-        std::cout << "\t" << "Error: " << error << std::endl;
+        std::cout << errorColor << "\t" << "Error: " << error << termcolor::reset << std::endl;
+
     if (result.returnValue)
-        std::cout << "\t" << "Return value: '" << *result.returnValue << "'" << std::endl;
+        std::cout << "\t" << "Returned '" << returnColor << *result.returnValue << termcolor::reset << "'" << std::endl;
+
     std::cout << std::endl;
 }
