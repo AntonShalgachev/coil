3bbaa139279a466271408db3809b817d998db758
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-01-30 23:17:37 +0100

    add user types to TypeSerializer tests

--- tests/cmake_tests/runtime/TypeSerializerTests.cpp
+++ tests/cmake_tests/runtime/TypeSerializerTests.cpp
@@ -1,5 +1,42 @@
 #include "gtest/gtest.h"
+#include "Common.h"
 #include "coil/TypeSerializer.h"
+
+namespace
+{
+    class WithoutDefaultConstructor
+    {
+    public:
+        WithoutDefaultConstructor(int value) : m_value(value) {}
+
+        int m_value = 0;
+    };
+
+    bool operator==(WithoutDefaultConstructor const& lhs, WithoutDefaultConstructor const& rhs)
+    {
+        return lhs.m_value == rhs.m_value;
+    }
+}
+
+namespace coil
+{
+    template<>
+    struct TypeSerializer<WithoutDefaultConstructor>
+    {
+        template<typename OnError>
+        static WithoutDefaultConstructor fromString(std::string_view str, OnError&& onError)
+        {
+            int innerValue = TypeSerializer<int>::fromString(str, onError);
+
+            return WithoutDefaultConstructor{ innerValue };
+        }
+
+        static std::string toString(WithoutDefaultConstructor const& value)
+        {
+            return "WithoutDefaultConstructor{" + std::to_string(value.m_value) + "}";
+        }
+    };
+}
 
 TEST(TypeSerializerTests, TestIntValidInputFromString)
 {
@@ -90,4 +127,33 @@ TEST(TypeSerializerTests, TestBoolToString)
 {
     EXPECT_EQ(coil::TypeSerializer<bool>::toString(true), "true");
     EXPECT_EQ(coil::TypeSerializer<bool>::toString(false), "false");
+}
+
+TEST(TypeSerializerTests, TestUserTypeFromStringValid)
+{
+    std::optional<std::string> error;
+    auto onError = [&error](auto value)
+    {
+        error = std::move(value);
+    };
+
+    EXPECT_EQ(coil::TypeSerializer<WithoutDefaultConstructor>::fromString("42", onError), WithoutDefaultConstructor{ 42 });
+}
+
+TEST(TypeSerializerTests, TestUserTypeFromStringInvalid)
+{
+    std::optional<std::string> error;
+    auto onError = [&error](auto value)
+    {
+        error = std::move(value);
+    };
+
+    EXPECT_EQ(coil::TypeSerializer<WithoutDefaultConstructor>::fromString("foo", onError), WithoutDefaultConstructor{ 0 });
+    EXPECT_TRUE(error.has_value());
+    EXPECT_EQ(*error, "Unable to convert 'foo' to type 'WithoutDefaultConstructor'");
+}
+
+TEST(TypeSerializerTests, TestUserTypeToString)
+{
+    EXPECT_EQ(coil::TypeSerializer<WithoutDefaultConstructor>::toString(WithoutDefaultConstructor{ 42 }), "WithoutDefaultConstructor{42}");
 }
