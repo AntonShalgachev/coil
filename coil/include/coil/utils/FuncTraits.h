309f824a07c9274638bcbfc18099f20459c4abbb
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-02-11 22:05:30 +0100

    simplify `FuncTraits`

--- coil/include/coil/utils/FuncTraits.h
+++ coil/include/coil/utils/FuncTraits.h
@@ -5,74 +5,71 @@
 
 namespace coil::utils
 {
-    template<typename R, typename T, bool IsObject, bool IsConst, typename... Args>
+    template<typename R, typename... Args>
     struct BaseFuncTraits
     {
     public:
         static constexpr bool isFunc = true;
 
         using ReturnType = R;
-        using ObjectType = T; // void if free/static function, T if it's a method of type T
         using ArgumentTypes = Types<Args...>;
-
-        static constexpr bool isConst = IsConst;
-        static constexpr bool isObject = IsObject;
-        static const bool isMethod = !isObject && !std::is_void_v<ObjectType>;
-
-        template<typename U>
-        static const bool isMethodOfType = !isObject && !std::is_void_v<U> && std::is_same_v<ObjectType, U>;
     };
 
     // General types
-    template<typename T, bool IsObject>
+    template<typename Func>
     struct FuncTraitsImpl
     {
         static constexpr bool isFunc = false;
     };
 
     // Free functions
-    template<typename R, typename... Args, bool IsObject>
-    struct FuncTraitsImpl<R(*)(Args...), IsObject> : public BaseFuncTraits<R, void, IsObject, false, Args...> {};
-    template<typename R, typename... Args, bool IsObject>
-    struct FuncTraitsImpl<R(*)(Args...) noexcept, IsObject> : public BaseFuncTraits<R, void, IsObject, false, Args...> {};
+    template<typename R, typename... Args>
+    struct FuncTraitsImpl<R(*)(Args...)> : public BaseFuncTraits<R, Args...> {};
+    template<typename R, typename... Args>
+    struct FuncTraitsImpl<R(*)(Args...) noexcept> : public BaseFuncTraits<R, Args...> {};
 
     // Pointer to member function
-#define COIL_MEMBER_FUNCTION_SPECIALIZATION(QUALIFIERS, IS_CONST) \
-    template<typename R, typename T, typename... Args, bool IsObject> \
-    struct FuncTraitsImpl<R(T::*)(Args...) QUALIFIERS, IsObject> : public BaseFuncTraits<R, T, IsObject, IS_CONST, Args...> {}
+#define COIL_MEMBER_FUNCTION_SPECIALIZATION(QUALIFIERS) \
+    template<typename R, typename T, typename... Args> \
+    struct FuncTraitsImpl<R(T::*)(Args...) QUALIFIERS> : public BaseFuncTraits<R, Args...> {}
 
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(&, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(&&, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&&, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(noexcept, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile noexcept, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(&noexcept, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile& noexcept, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(&& noexcept, false);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&& noexcept, false);
+#pragma warning(push)
+#pragma warning(disable: 4003) // to allow calling macro with no arguments
 
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&&, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&&, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const noexcept, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile noexcept, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const& noexcept, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile& noexcept, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&& noexcept, true);
-    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&& noexcept, true);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION();
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(&&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(&noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile& noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(&& noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(volatile&& noexcept);
+
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&&);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const& noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile& noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const&& noexcept);
+    COIL_MEMBER_FUNCTION_SPECIALIZATION(const volatile&& noexcept);
+
+#pragma warning(pop)
 
 #undef COIL_MEMBER_FUNCTION_SPECIALIZATION
 
-    template<typename T, typename = void>
-    struct FuncTraits : FuncTraitsImpl<T, false> {};
+    template<typename Func, typename = void>
+    struct FuncTraits : FuncTraitsImpl<Func> {};
 
     // TODO support qualified operator()
-    template<typename T>
-    struct FuncTraits<T, std::void_t<decltype(&T::operator())>> : public FuncTraitsImpl<decltype(&T::operator()), true> {};
+    template<typename Func>
+    struct FuncTraits<Func, std::void_t<decltype(&Func::operator())>> : public FuncTraitsImpl<decltype(&Func::operator())> {};
 }
