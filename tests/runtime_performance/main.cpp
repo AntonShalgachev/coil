cb62c332f4e3126d0f1a6e570a9a660a41fd169d
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-02-12 20:48:11 +0100

    fix runtime performance tests

--- tests/runtime_performance/main.cpp
+++ tests/runtime_performance/main.cpp
@@ -5,17 +5,6 @@
 namespace user
 {
     template<std::size_t I>
-    class Object
-    {
-    public:
-        template<std::size_t J>
-        double method(float, float)
-        {
-            return 3.14;
-        }
-    };
-
-    template<std::size_t I>
     double function(float, float)
     {
         return 3.14;
@@ -37,60 +26,16 @@ namespace util
     {
         return generateUniqueFunctionsImpl(std::make_index_sequence<N>{});
     }
-
-    template<std::size_t... Is>
-    std::tuple<user::Object<Is>...> createUniqueObjectsImpl(std::index_sequence<Is...>)
-    {
-        return std::tuple<user::Object<Is>...>{};
-    }
-
-    template<std::size_t N>
-    auto createUniqueObjects()
-    {
-        return createUniqueObjectsImpl(std::make_index_sequence<N>{});
-    }
-
-    template<std::size_t I>
-    using MethodT = decltype(&user::Object<I>::template method<0>);
-
-    template<std::size_t I, std::size_t... Js>
-    std::vector<MethodT<I>> generateUniqueMethodsForTypeImpl(std::index_sequence<Js...>)
-    {
-        return {&user::Object<I>::template method<Js>...};
-    }
-
-    template<std::size_t I, std::size_t M>
-    std::vector<MethodT<I>> generateUniqueMethodsForType()
-    {
-        return generateUniqueMethodsForTypeImpl<I>(std::make_index_sequence<M>{});
-    }
-
-    template<std::size_t M, std::size_t... Is>
-    auto generateUniqueMethodsImpl(std::index_sequence<Is...>)
-    {
-        return std::tuple<std::vector<MethodT<Is>>...>{generateUniqueMethodsForType<Is, M>()...};
-    }
-
-    template<std::size_t N, std::size_t M>
-    auto generateUniqueMethods()
-    {
-        return generateUniqueMethodsImpl<M>(std::make_index_sequence<N>{});
-    }
 }
 
 namespace
 {
     coil::Bindings cmd;
-    coil::ExecutionInput cmdInput{ "", "long_function_name_please0", {"3.14", "0.16"}, {} };
-    coil::ExecutionInput cmdInputObject{ "long_object_name_please0", "long_method_name_please0", {"3.14", "0.16"}, {} };
+    coil::ExecutionInput cmdInput{ {"long_function_name_please0"}, {"3.14", "0.16"}, {} };
 
     sol::state lua;
 
     std::size_t constexpr freeFuncsCount = 500;
-    std::size_t constexpr objectsCount = 10;
-    std::size_t constexpr methodsCount = 300;
-
-    auto objects = util::createUniqueObjects<objectsCount>();
 
     void prepare()
     {
@@ -102,43 +47,6 @@ namespace
             cmd[name] = functions[i];
             lua[name] = functions[i];
         }
-
-        auto addObject = [](auto&& object, std::size_t index)
-        {
-            auto name = "long_object_name_please" + std::to_string(index);
-            cmd.addObject(name, &object);
-            lua[name] = &object;
-        };
-
-        std::apply([addObject = std::move(addObject)](auto&&... object)
-        {
-            std::size_t index = 0;
-            (addObject(object, index++), ...);
-        }, objects);
-
-        auto methods = util::generateUniqueMethods<objectsCount, methodsCount>();
-
-        auto addMethods = [](auto&& methods)
-        {
-            using Traits = coil::utils::FuncTraits<std::decay_t<decltype(methods[0])>>;
-
-            auto usertype = lua.new_usertype<typename Traits::ObjectType>("Object", sol::no_constructor);
-
-            for (std::size_t i = 0; i < methods.size(); i++)
-            {
-                auto const& method = methods[i];
-
-                auto name = "long_method_name_please" + std::to_string(i);
-                cmd.bind<typename Traits::ObjectType>(name, method);
-
-                usertype[name] = method;
-            }
-        };
-
-        std::apply([addMethods = std::move(addMethods)](auto&&... methods)
-        {
-            (addMethods(methods), ...);
-        }, methods);
     }
 
     int runs = 1000;
@@ -149,19 +57,9 @@ namespace
         cmd.execute("long_function_name_please0 3.14 0.16");
     }
 
-    BENCHMARK(Scripting, Coil_Object, runs, iterations)
-    {
-        cmd.execute("long_object_name_please0.long_method_name_please0 3.14 0.16");
-    }
-
     BENCHMARK(Scripting, CoilNoParse, runs, iterations)
     {
         cmd.execute(cmdInput);
-    }
-
-    BENCHMARK(Scripting, CoilNoParse_Object, runs, iterations)
-    {
-        cmd.execute(cmdInputObject);
     }
 
     BENCHMARK(Scripting, Sol, runs, iterations)
@@ -169,16 +67,13 @@ namespace
         lua.script("long_function_name_please0(3.14, 0.16)");
     }
 
-    BENCHMARK(Scripting, Sol_Object, runs, iterations)
-    {
-        lua.script("long_object_name_please0:long_method_name_please0(3.14, 0.16)");
-    }
-
     BENCHMARK(Scripting, DirectFromString, runs, iterations)
     {
         using TS = coil::TypeSerializer<float>;
-        auto onError = [](auto) {};
-        user::function<0>(TS::fromString("3.14", onError), TS::fromString("0.16", onError));
+        auto arg1 = TS::fromString("3.14");
+        auto arg2 = TS::fromString("0.16");
+        if (arg1 && arg2)
+            user::function<0>(*arg1, *arg2);
     }
 }
 
