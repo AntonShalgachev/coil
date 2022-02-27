2391c629613de5c03afec79085373050947ef313
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-02-27 22:32:11 +0100

    reuse memory in `formatString`

--- coil/include/coil/Utils.h
+++ coil/include/coil/Utils.h
@@ -1,20 +1,21 @@
 #pragma once
 
 #include <vector>
-#include <memory>
 #include <string>
 
 namespace coil
 {
-	template<typename... Args>
-	std::string formatString(char const* format, Args... args)
-	{
-		std::size_t size = static_cast<std::size_t>(std::snprintf(nullptr, 0, format, args...)) + 1;
+    template<typename... Args>
+    std::string formatString(char const* format, Args... args)
+    {
+        std::size_t size = static_cast<std::size_t>(std::snprintf(nullptr, 0, format, args...)) + 1;
 
-        // TODO use static std::vector<char>
-		auto buf = std::make_unique<char[]>(size);
-		std::snprintf(buf.get(), size, format, std::forward<Args>(args)...);
+        static std::vector<char> buf;
+        if (buf.size() < size)
+            buf.resize(size);
 
-		return std::string(buf.get(), buf.get() + size - 1);
-	}
+        std::snprintf(buf.data(), size, format, std::forward<Args>(args)...);
+
+        return std::string(buf.begin(), buf.begin() + size - 1);
+    }
 }
