fe02ce8c36a724b674d554b545760a71e7d14a44
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-03-23 15:34:52 +0100

    add more tests

--- tests/cmake_tests/runtime/ExpectedTests.cpp
+++ tests/cmake_tests/runtime/ExpectedTests.cpp
@@ -53,6 +53,8 @@ namespace
             return m_payload == rhs.m_payload;
         }
 
+        T const& payload() const { return m_payload; }
+
     private:
         T m_payload;
 
@@ -373,4 +375,13 @@ TEST(ExpectedTests, TestUnexpectedAccessRvalue)
     }, coil::BadExpectedAccess<E>);
 
     EXPECT_EQ(E::copies, 0);
+}
+
+TEST(ExpectedTests, TestExpectedArrow)
+{
+    coil::Expected<V, E> e1 = V{ 42 };
+    coil::Expected<V, E> const e2 = V{ 42 };
+
+    EXPECT_EQ(e1->payload(), 42);
+    EXPECT_EQ(e2->payload(), 42);
 }
