205b39f4fcb4256eb91042a085ef1086f47583cc
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-03-25 16:17:54 +0100

    merge several explicit template macros

--- coil/include/coil/Extern.h
+++ coil/include/coil/Extern.h
@@ -19,32 +19,32 @@ extern template class coil::BindingProxy<coil::Bindings>;
 
 extern template class std::vector<std::string_view>;
 
-#define EXTERN_EXPLICIT_NAMED_ARGS_TEMPLATE(T) \
+#define COIL_EXTERN_TEMPLATE(T) \
+    extern template auto coil::variable<T>(T* var); \
+    \
     extern template class std::optional<T>; \
     extern template class coil::ExpectedBase<T, coil::NamedArgs::Error>; \
     extern template class coil::Expected<T, coil::NamedArgs::Error>; \
     extern template coil::Expected<T, coil::NamedArgs::Error> coil::NamedArgs::get<T>(std::string_view key) const; \
-    extern template std::optional<T> coil::NamedArgs::getOrReport(std::string_view key, coil::NamedArgs::ArgType argType, std::optional<T> defaultValue) const
-
-#define EXPLICIT_NAMED_ARGS_TEMPLATE(T) \
-    template class std::optional<T>; \
-    template class coil::ExpectedBase<T, coil::NamedArgs::Error>; \
-    template class coil::Expected<T, coil::NamedArgs::Error>; \
-    template coil::Expected<T, coil::NamedArgs::Error> coil::NamedArgs::get<T>(std::string_view key) const; \
-    template std::optional<T> coil::NamedArgs::getOrReport(std::string_view key, coil::NamedArgs::ArgType argType, std::optional<T> defaultValue) const
-
-#define EXTERN_EXPLICIT_FUNCTOR_CALLER_TEMPLATE(T) \
-    extern template void coil::detail::reportError<T>(coil::detail::CallContext& context, coil::Expected<T, std::string> const& result)
-
-#define EXPLICIT_FUNCTOR_CALLER_TEMPLATE(T) \
-    template void coil::detail::reportError<T>(coil::detail::CallContext& context, coil::Expected<T, std::string> const& result)
-
-#define EXTERN_EXPLICIT_TYPE_SERIALIZER_TEMPLATE(T) \
+    extern template std::optional<T> coil::NamedArgs::getOrReport(std::string_view key, coil::NamedArgs::ArgType argType, std::optional<T> defaultValue) const; \
+    \
+    extern template void coil::detail::reportError<T>(coil::detail::CallContext& context, coil::Expected<T, std::string> const& result); \
+    \
     extern template struct coil::TypeSerializer<T>; \
     extern template class coil::ExpectedBase<T, std::string>; \
     extern template class coil::Expected<T, std::string>
 
-#define EXPLICIT_TYPE_SERIALIZER_TEMPLATE(T) \
+#define COIL_TEMPLATE(T) \
+    template auto coil::variable<T>(T* var); \
+    \
+    template class std::optional<T>; \
+    template class coil::ExpectedBase<T, coil::NamedArgs::Error>; \
+    template class coil::Expected<T, coil::NamedArgs::Error>; \
+    template coil::Expected<T, coil::NamedArgs::Error> coil::NamedArgs::get<T>(std::string_view key) const; \
+    template std::optional<T> coil::NamedArgs::getOrReport(std::string_view key, coil::NamedArgs::ArgType argType, std::optional<T> defaultValue) const; \
+    \
+    template void coil::detail::reportError<T>(coil::detail::CallContext& context, coil::Expected<T, std::string> const& result); \
+    \
     template struct coil::TypeSerializer<T>; \
     template class coil::ExpectedBase<T, std::string>; \
     template class coil::Expected<T, std::string>
