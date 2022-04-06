a5341b48f171a1669a6843a5b25b3ce6a8c151b8
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-04-06 23:07:25 +0200

    try to fix Ubuntu

--- coil/include/coil/Coil.h
+++ coil/include/coil/Coil.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef COIL_NO_EXTERN_TEMPLATES
+#define _COIL_HAS_EXTERN_TEMPLATES 0
+#else
+#define _COIL_HAS_EXTERN_TEMPLATES 1
+#endif
+
 #include "coil/AnyArgView.h"
 #include "coil/Bind.h"
 #include "coil/Bindings.h"
