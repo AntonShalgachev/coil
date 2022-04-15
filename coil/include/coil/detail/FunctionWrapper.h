444e3ebf0ef5760eeffa2f2ef0c1eff30d2dcb7e
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-04-15 20:54:19 +0200

    remove MemberFunctionFunctor

--- coil/include/coil/detail/FunctionWrapper.h
+++ coil/include/coil/detail/FunctionWrapper.h
@@ -12,16 +12,17 @@ namespace coil::detail
     public:
         using ArgsTraits = ArgsTraitsImpl<std::decay_t<Args>...>; // TODO simplier decay
 
-        template<typename Func>
-        FunctionWrapper(Func func)
+        template<typename Func, typename C = void>
+        FunctionWrapper(Func func, C* obj = nullptr)
         {
             using UnqualifiedFunc = std::decay_t<Func>;
 
-            static_assert(!std::is_member_function_pointer_v<UnqualifiedFunc>, "Func shouldn't be a member function");
+            static_assert(!std::is_member_function_pointer_v<UnqualifiedFunc> || !std::is_void_v<C>, "Func can only be a member function if C isn't void");
 
             m_func = new UnqualifiedFunc(std::move(func));
+            m_obj = const_cast<std::remove_const_t<C>*>(obj); // this pointer would be casted back to C* in typedCall
 
-            m_callFunc = &FunctionWrapper<Args...>::typedCall<UnqualifiedFunc>;
+            m_callFunc = &FunctionWrapper<Args...>::typedCall<UnqualifiedFunc, C>;
             m_destroyFunc = &FunctionWrapper<Args...>::typedDestroy<UnqualifiedFunc>;
         }
         ~FunctionWrapper();
@@ -39,19 +40,26 @@ namespace coil::detail
         using CallFuncPtr = std::optional<std::string>(FunctionWrapper::*)(Args... args);
         using DestroyFuncPtr = void(FunctionWrapper::*)();
 
-        template<typename Func>
+        template<typename Func, typename C>
         std::optional<std::string> typedCall(Args... args)
         {
-            Func* func = static_cast<Func*>(m_func);
+            Func& func = *static_cast<Func*>(m_func);
+            C* obj = static_cast<C*>(m_obj);
             using R = typename FuncTraits<Func>::ReturnType;
             if constexpr (std::is_void_v<R>)
             {
-                (*func)(std::move(args)...);
+                if constexpr (std::is_void_v<C>)
+                    func(std::move(args)...);
+                else
+                    (obj->*func)(std::move(args)...);
                 return {};
             }
             else
             {
-                return TypeSerializer<R>::toString((*func)(std::move(args)...));
+                if constexpr (std::is_void_v<C>)
+                    return TypeSerializer<R>::toString(func(std::move(args)...));
+                else
+                    return TypeSerializer<R>::toString((obj->*func)(std::move(args)...));
             }
         }
 
@@ -65,6 +73,7 @@ namespace coil::detail
 
     private:
         void* m_func = nullptr;
+        void* m_obj = nullptr;
         CallFuncPtr m_callFunc = nullptr;
         DestroyFuncPtr m_destroyFunc = nullptr;
     };
@@ -75,6 +84,7 @@ namespace coil::detail
         m_func = rhs.m_func;
         rhs.m_func = nullptr;
 
+        m_obj = rhs.m_obj;
         m_callFunc = rhs.m_callFunc;
         m_destroyFunc = rhs.m_destroyFunc;
     }
