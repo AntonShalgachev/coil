814e107fd50173f514132e5ea4588c031e0c4914
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-06-17 21:29:12 +0200

    add clang-format

---
+++ clang-format-all.ps1
@@ -0,0 +1,21 @@
+function Format-Directory {
+    param (
+        [string]$Path,
+        $Filter = {$True}
+    )
+
+    $DirInfos = @(Get-Item -Path $Path)
+    $DirInfos += Get-ChildItem -Path $Path -Directory -Recurse
+    $DirInfos | Where-Object $Filter |
+    ForEach-Object {
+        Write-Host $_.FullName
+        Push-Location $_.FullName
+        &clang-format -i *.h *.cpp
+        Pop-Location
+    }
+}
+
+Format-Directory src
+Format-Directory include
+Format-Directory examples
+Format-Directory tests {$_.FullName -notlike "*build*" -And $_.FullName -notlike "*generated*" -And $_.FullName -notlike "*scripts*"}
