4e28bbea1952b3b6a0bd8b2513220aa21cc4ceed
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-06-24 21:43:12 +0200

    remove flags from VariableExample

--- examples/variables/VariablesExample.cpp
+++ examples/variables/VariablesExample.cpp
@@ -1,25 +1,17 @@
 #include "VariablesExample.h"
 
 #include "common/ExamplesCommon.h"
-#include "common/FlagVariableWrapper.h"
 
 #include <iostream>
 
 namespace
 {
-    enum class Flags
+    enum class Type
     {
-        Flag1 = 0b0001,
-        Flag2 = 0b0010,
-        Flag3 = 0b0100,
-        Flag4 = 0b1000,
+        Type1,
+        Type2,
+        Type3
     };
-
-    Flags operator|(Flags lhs, Flags rhs)
-    {
-        using UT = std::underlying_type_t<Flags>;
-        return static_cast<Flags>(static_cast<UT>(lhs) | static_cast<UT>(rhs));
-    }
 }
 
 void VariablesExample::run()
@@ -28,32 +20,31 @@ void VariablesExample::run()
 
     float timeScale = 1.0f;
     std::string city = "Moscow";
-    Flags flags = Flags::Flag1 | Flags::Flag3;
+    Type type = Type::Type2;
 
     bindings["time_scale"] = coil::variable(&timeScale);
     bindings["city"] = coil::variable(&city);
-    bindings["flags"] = ::flagsVariable(&flags); // This allows to OR flags when invoking the command; see the description of this function
+    bindings["type"] = coil::variable(&type);
 
-    bindings["print_variables"] = [&timeScale, &city, &flags](coil::Context context) {
-        context.out() << "Time scale: " << timeScale << "; city: " << city << "; flags: " << magic_enum::flags::enum_name(flags) << std::endl;
+    bindings["print_variables"] = [&timeScale, &city, &type](coil::Context context) {
+        context.out() << "Time scale: " << timeScale << "; city: " << city << "; type: " << magic_enum::enum_name(type) << std::endl;
     };
 
     common::printSectionHeader("Calling variable without arguments will return its value:");
     common::executeCommand(bindings, "time_scale");
     common::executeCommand(bindings, "city");
-    common::executeCommand(bindings, "flags");
+    common::executeCommand(bindings, "type");
     common::executeCommand(bindings, "print_variables");
 
     common::printSectionHeader("Calling variable with an argument will update and return the value:");
     common::executeCommand(bindings, "time_scale 2.0");
     common::executeCommand(bindings, "city Berlin");
-    common::executeCommand(bindings, "flags Flag1|Flag2|Flag4");
+    common::executeCommand(bindings, "type Type3");
     common::executeCommand(bindings, "print_variables");
 
     common::printSectionHeader("Errors are handled just as for functions (variables won't change):");
     common::executeCommand(bindings, "time_scale 2.0 3.0");
     common::executeCommand(bindings, "city Porto Athens");
-    common::executeCommand(bindings, "flags Flag1 Flag2"); // considered to be 2 arguments, therefore error
-    common::executeCommand(bindings, "flags Foo|Flag4|Bar");
+    common::executeCommand(bindings, "type UnknownType");
     common::executeCommand(bindings, "print_variables");
 }
