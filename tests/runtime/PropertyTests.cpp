02eeb3c9387dc5f2de163f40f79587908b833f03
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-06-27 22:00:49 +0200

    add tests for read-only properties

--- tests/runtime/PropertyTests.cpp
+++ tests/runtime/PropertyTests.cpp
@@ -11,6 +11,11 @@ namespace
 
         return coil::property(get, set);
     }
+    auto createReadonlyProperty(int& variable)
+    {
+        auto get = [&variable]() { return variable; };
+        return coil::property(get);
+    }
 
     struct Object
     {
@@ -60,6 +65,34 @@ TEST(PropertyTests, TestValidSet)
     EXPECT_EQ(variable, 42);
 }
 
+TEST(PropertyTests, TestReadonlyGet)
+{
+    int variable = 17;
+
+    coil::Bindings bindings;
+    bindings["func"] = createReadonlyProperty(variable);
+
+    auto result = bindings.execute("func");
+    EXPECT_EQ(result.errors.size(), 0u);
+
+    ASSERT_TRUE(result.returnValue);
+    EXPECT_EQ(*result.returnValue, "17");
+    EXPECT_EQ(variable, 17);
+}
+
+TEST(PropertyTests, TestReadonlySet)
+{
+    int variable = 17;
+
+    coil::Bindings bindings;
+    bindings["func"] = createReadonlyProperty(variable);
+
+    auto result = bindings.execute("func 42");
+    EXPECT_EQ(result.errors.size(), 1u);
+    EXPECT_PRED2(containsError, result.errors, "This property is read-only");
+    EXPECT_EQ(variable, 17);
+}
+
 TEST(PropertyTests, TestWrongArguments)
 {
     int variable = 17;
@@ -101,6 +134,34 @@ TEST(PropertyTests, TestValidSetOnObject)
     ASSERT_TRUE(result.returnValue);
     EXPECT_EQ(*result.returnValue, "42");
     EXPECT_EQ(object.get(), 42);
+}
+
+TEST(PropertyTests, TestReadonlyGetOnObject)
+{
+    Object object = 17;
+
+    coil::Bindings bindings;
+    bindings["func"] = coil::bindProperty(&Object::get, &object);
+
+    auto result = bindings.execute("func");
+    EXPECT_EQ(result.errors.size(), 0u);
+
+    ASSERT_TRUE(result.returnValue);
+    EXPECT_EQ(*result.returnValue, "17");
+    EXPECT_EQ(object.get(), 17);
+}
+
+TEST(PropertyTests, TestReadonlySetOnObject)
+{
+    Object object = 17;
+
+    coil::Bindings bindings;
+    bindings["func"] = coil::bindProperty(&Object::get, &object);
+
+    auto result = bindings.execute("func 42");
+    EXPECT_EQ(result.errors.size(), 1u);
+    EXPECT_PRED2(containsError, result.errors, "This property is read-only");
+    EXPECT_EQ(object.get(), 17);
 }
 
 TEST(PropertyTests, TestWrongArgumentsOnObject)
