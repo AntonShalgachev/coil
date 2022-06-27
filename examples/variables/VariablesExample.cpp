b1b09c65813fda4f783c5c8c4d9df235899a69ad
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-06-27 21:14:48 +0200

    add example of `toggle` for booleans

--- examples/variables/VariablesExample.cpp
+++ examples/variables/VariablesExample.cpp
@@ -12,6 +12,22 @@ namespace
         Type2,
         Type3
     };
+
+    auto toggle(bool* var)
+    {
+        auto toggler = [var]()
+        {
+            *var = !*var;
+            return *var;
+        };
+        auto setter = [var](bool val)
+        {
+            *var = std::move(val);
+            return *var;
+        };
+
+        return coil::overloaded(std::move(toggler), std::move(setter));
+    }
 }
 
 void VariablesExample::run()
@@ -21,10 +37,13 @@ void VariablesExample::run()
     float timeScale = 1.0f;
     std::string city = "Moscow";
     Type type = Type::Type2;
+    bool godMode = false;
 
     bindings["time_scale"] = coil::variable(&timeScale);
     bindings["city"] = coil::variable(&city);
     bindings["type"] = coil::variable(&type);
+    bindings["god_var"] = coil::variable(&godMode);
+    bindings["god_toggle"] = ::toggle(&godMode);
 
     bindings["print_variables"] = [&timeScale, &city, &type](coil::Context context) {
         context.out() << "Time scale: " << timeScale << "; city: " << city << "; type: " << magic_enum::enum_name(type) << std::endl;
@@ -47,4 +66,10 @@ void VariablesExample::run()
     common::executeCommand(bindings, "city Porto Athens");
     common::executeCommand(bindings, "type UnknownType");
     common::executeCommand(bindings, "print_variables");
+
+    common::printSectionHeader("You can create your own variable-like commands");
+    common::executeCommand(bindings, "god_var");
+    common::executeCommand(bindings, "god_toggle"); // toggles the bool instead of just returning the value
+    common::executeCommand(bindings, "god_toggle false");
+    common::executeCommand(bindings, "god_toggle 1");
 }
