244ce686f74fb81f22a8eda93c4248de83993b60
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-06-28 12:02:10 +0200

    add variables to the enums example

--- examples/enums/EnumsExample.cpp
+++ examples/enums/EnumsExample.cpp
@@ -52,11 +52,21 @@ void EnumsExample::run()
     coil::Bindings bindings;
     PlayerAbilitySystem abilities;
 
+    enum class VariableType
+    {
+        Integer,
+        Float,
+        String,
+    };
+
+    VariableType variableType = VariableType::String;
+
     // See EnumTypeSerializer.h to see how the serialization is implemented
 
     bindings["abilities.set_ability"] = coil::bind(&PlayerAbilitySystem::setAbility, &abilities);
     bindings["abilities.get_ability"] = coil::bind(&PlayerAbilitySystem::getAbility, &abilities);
     bindings["abilities.get_reset_mode"] = coil::bind(&PlayerAbilitySystem::getResetMode, &abilities);
+    bindings["variable_type"] = coil::variable(&variableType);
 
     common::printSectionHeader("Enums can be used as long as a corresponding TypeSerializer is specialized:");
     common::executeCommand(bindings, "abilities.set_ability SpeedBoost AfterDelay");
@@ -64,4 +74,8 @@ void EnumsExample::run()
     common::printSectionHeader("Both scoped and unscoped enums can be used:");
     common::executeCommand(bindings, "abilities.get_ability");
     common::executeCommand(bindings, "abilities.get_reset_mode");
+
+	common::printSectionHeader("Enum variables can also be used:");
+	common::executeCommand(bindings, "variable_type");
+	common::executeCommand(bindings, "variable_type Float");
 }
