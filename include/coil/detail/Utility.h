3e287cb80c5da9696327caa67b473bbfd4351171
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-10-09 02:20:49 +0200

    implement `Forward`

--- include/coil/detail/Utility.h
+++ include/coil/detail/Utility.h
@@ -4,9 +4,21 @@
 
 namespace coil
 {
-    template <class T>
+    template <typename T>
     constexpr RemoveReferenceT<T>&& Move(T&& arg) noexcept
     {
         return static_cast<RemoveReferenceT<T>&&>(arg);
+    }
+
+    template <typename T>
+    constexpr T&& Forward(RemoveReferenceT<T>& arg) noexcept
+    {
+        return static_cast<T&&>(arg);
+    }
+
+    template <typename T>
+    constexpr T&& Forward(RemoveReferenceT<T>&& arg) noexcept
+    {
+        return static_cast<T&&>(arg);
     }
 }
