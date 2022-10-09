98bb12fba808dfbc7bd4aee28800c43eb2fe3a8f
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-10-09 12:22:40 +0200

    remove `Expected<void>`

--- include/coil/Expected.h
+++ include/coil/Expected.h
@@ -50,14 +50,6 @@ namespace coil
         T m_value;
     };
 
-    namespace detail
-    {
-        // aka std::monostate
-        struct dummy
-        {
-        };
-    }
-
     template<typename T>
     Unexpected<T> makeUnexpected(T value)
     {
@@ -65,35 +57,36 @@ namespace coil
     }
 
     template<typename T, typename E>
-    class ExpectedBase
+    class Expected
     {
+        static_assert(!IsVoidV<T>, "T can't be void");
         static_assert(!IsVoidV<E>, "E can't be void");
 
     public:
-        ExpectedBase(T value) : m_expected(Move(value)), m_hasValue(true) {}
+        Expected(T value) : m_expected(Move(value)), m_hasValue(true) {}
 
         template<typename U>
-        ExpectedBase(Unexpected<U> error) : m_unexpected(Move(error)), m_hasValue(false)
+        Expected(Unexpected<U> error) : m_unexpected(Move(error)), m_hasValue(false)
         {
             
         }
 
-        ExpectedBase(ExpectedBase<T, E> const& rhs)
+        Expected(Expected<T, E> const& rhs)
         {
             constructFrom(rhs);
         }
 
-        ExpectedBase(ExpectedBase<T, E>&& rhs) noexcept
+        Expected(Expected<T, E>&& rhs) noexcept
         {
             constructFrom(Move(rhs));
         }
 
-        ~ExpectedBase() // not virtual
+        ~Expected() // not virtual
         {
             destruct();
         }
 
-        ExpectedBase<T, E>& operator=(ExpectedBase<T, E> const& rhs)
+        Expected<T, E>& operator=(Expected<T, E> const& rhs)
         {
             if (m_hasValue && rhs.m_hasValue)
             {
@@ -112,7 +105,7 @@ namespace coil
             return *this;
         }
 
-        ExpectedBase<T, E>& operator=(ExpectedBase<T, E>&& rhs) noexcept
+        Expected<T, E>& operator=(Expected<T, E>&& rhs) noexcept
         {
             if (m_hasValue && rhs.m_hasValue)
             {
@@ -157,6 +150,66 @@ namespace coil
             return Move(m_unexpected).value();
         }
 
+        template<typename T2, typename E2>
+        bool operator==(Expected<T2, E2> const& rhs) const
+        {
+            static_assert(!IsVoidV<T2>, "Can't compare T with void");
+
+            if (hasValue() && rhs.hasValue())
+                return value() == rhs.value();
+
+            if (!hasValue() && !rhs.hasValue())
+                return error() == rhs.error();
+
+            return false;
+        }
+
+        template<typename T2>
+        bool operator==(T2 const& rhs) const
+        {
+            return hasValue() && value() == rhs;
+        }
+
+        T const& value() const&
+        {
+            assert(hasValue());
+            return m_expected;
+        }
+
+        T& value()&
+        {
+            assert(hasValue());
+            return m_expected;
+        }
+
+        T&& value()&&
+        {
+            assert(hasValue());
+            return Move(*this).m_expected;
+        }
+
+        T const& operator*() const&
+        {
+            return value();
+        }
+        T& operator*()&
+        {
+            return value();
+        }
+        T&& operator*()&&
+        {
+            return Move(*this).value();
+        }
+
+        T const* operator->() const
+        {
+            return &value();
+        }
+        T* operator->()
+        {
+            return &value();
+        }
+
         template<typename E2>
         bool operator==(Unexpected<E2> const& rhs) const
         {
@@ -164,7 +217,7 @@ namespace coil
         }
 
     protected:
-        void constructFrom(ExpectedBase<T, E> const& rhs)
+        void constructFrom(Expected<T, E> const& rhs)
         {
             m_hasValue = rhs.m_hasValue;
             if (rhs.m_hasValue)
@@ -173,7 +226,7 @@ namespace coil
                 new (&m_unexpected) Unexpected<E>(rhs.m_unexpected);
         }
 
-        void constructFrom(ExpectedBase<T, E>&& rhs)
+        void constructFrom(Expected<T, E>&& rhs)
         {
             m_hasValue = rhs.m_hasValue;
             if (rhs.m_hasValue)
@@ -196,141 +249,5 @@ namespace coil
             Unexpected<E> m_unexpected;
         };
         bool m_hasValue = true;
-    };
-
-    template<typename T, typename E>
-    class Expected final : public ExpectedBase<T, E>
-    {
-    public:
-        using Base = ExpectedBase<T, E>;
-
-        using Base::Base;
-        using Base::operator bool;
-        using Base::operator==;
-
-        Expected(T value) : Base(Move(value)) {}
-
-        Expected(Expected<T, E> const& rhs) : Base(rhs) {}
-
-        Expected(Expected<T, E>&& rhs) noexcept : Base(Move(rhs)) {}
-
-        ~Expected(){}; // not default to enable explicit instantiation
-
-        Expected<T, E>& operator=(Expected<T, E> const& rhs)
-        {
-            return static_cast<Expected<T, E>&>(Base::operator=(rhs));
-        }
-
-        Expected<T, E>& operator=(Expected<T, E>&& rhs) noexcept
-        {
-            return static_cast<Expected<T, E>&>(Base::operator=(Move(rhs)));
-        }
-
-        template<typename T2, typename E2>
-        bool operator==(Expected<T2, E2> const& rhs) const
-        {
-            static_assert(!IsVoidV<T2>, "Can't compare T with void");
-
-            if (this->hasValue() && rhs.hasValue())
-                return value() == rhs.value();
-
-            if (!this->hasValue() && !rhs.hasValue())
-                return this->error() == rhs.error();
-
-            return false;
-        }
-
-        template<typename T2>
-        bool operator==(T2 const& rhs) const
-        {
-            return this->hasValue() && value() == rhs;
-        }
-
-        T const& value() const&
-        {
-            assert(this->hasValue());
-            return this->m_expected;
-        }
-
-        T& value() &
-        {
-            assert(this->hasValue());
-            return this->m_expected;
-        }
-
-        T&& value() &&
-        {
-            assert(this->hasValue());
-            return Move(*this).m_expected;
-        }
-
-        T const& operator*() const&
-        {
-            return value();
-        }
-        T& operator*() &
-        {
-            return value();
-        }
-        T&& operator*() &&
-        {
-            return Move(*this).value();
-        }
-
-        T const* operator->() const
-        {
-            return &value();
-        }
-        T* operator->()
-        {
-            return &value();
-        }
-    };
-
-    template<typename E>
-    class Expected<void, E> final : public ExpectedBase<detail::dummy, E>
-    {
-    public:
-        using Base = ExpectedBase<detail::dummy, E>;
-
-        using Base::Base;
-        using Base::operator bool;
-        using Base::operator==;
-
-        Expected() : Base(detail::dummy{}) {}
-
-        Expected(Expected<void, E> const& rhs) : Base(rhs)
-        {
-            constructFrom(rhs);
-        }
-
-        Expected(Expected<void, E>&& rhs) noexcept : Base(Move(rhs))
-        {
-            constructFrom(Move(rhs));
-        }
-
-        ~Expected(){}; // not default to enable explicit instantiation
-
-        Expected<void, E>& operator=(Expected<void, E> const& rhs)
-        {
-            return static_cast<Expected<void, E>&>(Base::operator=(rhs));
-        }
-
-        Expected<void, E>& operator=(Expected<void, E>&& rhs) noexcept
-        {
-            return static_cast<Expected<void, E>&>(Base::operator=(Move(rhs)));
-        }
-
-        template<typename E2>
-        bool operator==(Expected<void, E2> const& rhs) const
-        {
-            if (this->hasValue() && rhs.hasValue())
-                return true;
-
-            if (!this->hasValue() && !rhs.hasValue())
-                return this->error() == rhs.error();
-
-            return false;
-        }
     };
 }
