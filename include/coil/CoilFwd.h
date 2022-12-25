74dbb7ee4ed79b9ed4aef57596377497294ee4be
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-12-25 17:57:42 +0100

    add more forward-declarations

--- include/coil/CoilFwd.h
+++ include/coil/CoilFwd.h
@@ -2,9 +2,22 @@
 
 namespace coil
 {
+    class Bindings;
+
+    class Context;
+    struct Value;
+    struct ExecutionInput;
+    struct ExecutionResult;
+
     template<typename T, typename>
     struct TypeName;
 
     template<typename T, typename>
     struct TypeSerializer;
+
+    template<typename T, typename E>
+    class Expected;
+
+    class String;
+    class StringView;
 }
